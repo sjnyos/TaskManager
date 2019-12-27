07e0386b50a74ae5eff5f777ea1a378758802982
sjnyos <wrksuraj@gmail.com>
2019-12-27 08:00:23 +0545

    Initial commit

---
+++ app/src/main/java/com/machamasisuraj/taksmanager/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.machamasisuraj.taksmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText,editText2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button= findViewById(R.id.bottom);
+        editText= findViewById(R.id.editText);
+        editText2=findViewById(R.id.editText2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
